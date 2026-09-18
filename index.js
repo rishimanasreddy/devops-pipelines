b585270352959f165bda26d22bd227d7bb6151ae
rishimanasreddy <rishimanasreddy@gmail.com>
2026-09-18 14:49:58 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, Devops!");
+console.log("I am learning DevOps and CI/CD pipelines.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting.....");
+setTimeout(() => {
+  console.log("Waiting for 3 seconds...");
+}, 3000);
+console.log("Test completed.");
